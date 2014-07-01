47d60831936f8b27564b3c3277bcf92b891fda4f
Mitar <mitar.git@tnode.com>
2014-07-01 01:39:22 -0700

    Bind DNS only to the external port because there is also Skydock bound to an internal IP.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 53:53/tcp -p 53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
+docker run -d -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
