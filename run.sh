1364190714d376c79fa00830b43d7967b9cf7ce0
Mitar <mitar.git@tnode.com>
2014-07-03 02:56:15 -0700

    Set hostname.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/bash -e
 
-docker run -d -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
+docker run -d -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns -h dns.postfix.server2.docker -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
