d3054110a06e251408c235c7aba3b48d25830430
Mitar <mitar.git@tnode.com>
2014-07-06 20:28:48 -0700

    Prepare directories.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #/bin/bash -e
 
+mkdir -p /var/log/powerdns
+chown nobody:nogroup /var/log/powerdns
+
 docker run -d -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns -h dns.postfix.server2.docker -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
