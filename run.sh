2d4121f865c77f5f3671db4f3e1a23043afd953c
Mitar <mitar.git@tnode.com>
2014-12-16 10:22:23 -0800

    Upgrading Docker and moving to docker-hosts.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/log/powerdns
 
-docker run -d -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns -h dns.postfix.server2.docker -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
+docker run -d --restart=always -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns --hostname dns -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
