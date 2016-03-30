5848dbc220d2edb2401063c2e48ad744df26afd4
Mitar <mitar.git@tnode.com>
2016-03-30 04:18:44 -0700

    New IP range.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/log/powerdns
 
-docker run -d --restart=always -p 50.0.115.228:53:53/tcp -p 50.0.115.228:53:53/udp --name dns --hostname dns -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
+docker run -d --restart=always -p 64.62.133.44:53:53/tcp -p 64.62.133.44:53:53/udp --name dns --hostname dns -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-master
